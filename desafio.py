78024bbdbb6f10c3475393ee5528a60be74c3ee3
Gabriel Macedo <84541578+MGabrielSantos@users.noreply.github.com>
2023-06-26 17:13:04 -0300

    Adicionando arquivos
    Adicionando arquivos para criação do novo repositório

---
+++ desafio.py
@@ -0,0 +1,10 @@
+usuario = input("Informe o usuário do sistema: ")
+
+if(usuario == "Gabriel"):
+    print("Seja bem-vindo Gabriel!")
+elif(usuario == "Douglas"):
+    print("Seja bem-vindo Douglas!")
+elif(usuario == "Nico"):
+    print("Seja bem-vindo Nico")
+else:
+    print("Usuário não identificado!")
